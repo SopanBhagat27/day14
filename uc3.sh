361e14a905bca6504d89659d1e04011e8c1c88ac
SopanBhagat27 <41367620+SopanBhagat27@users.noreply.github.com>
2020-07-08 23:44:43 +0530

    Add files via upload
    email code

---
+++ uc3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+shopt -s extglob
+
+read -p "Enter the Email Id:" email
+patern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,5}([.][a-zA-Z]{2})*$"
+
+if [[ $email =~ $patern ]]
+then
+	echo "Valid"
+else
+	echo "Invalid "
+fi
